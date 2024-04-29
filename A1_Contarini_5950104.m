d163e84f995904d81dc7dc1e202238631c019fc5
mcontarini <mcontarini@student.tudelft.nl>
2024-04-29 10:20:25 +0200

    Added code for task 7

--- A1_Contarini_5950104.m
+++ A1_Contarini_5950104.m
@@ -25,7 +25,7 @@ Md = [0.001; 0.001; 0.001];
 
 theta_ss = deg2rad([0.1; 0.1; 0.1]); % Required steady-state error
 
-angular_velocity_measurement_errors = deg2rad([0.1 -0.1 0.15]);
+angular_velocity_measurement_errors = deg2rad([0.1; -0.1; 0.15]);
 
 damping_ratio = [0.707; 0.707; 0.707];
 
@@ -131,7 +131,115 @@ hold off
 
 %% Task 7
 
+N = length(t2);
 
+b_1_0 = deg2rad(5);
+b_2_0 = deg2rad(5);
+b_3_0 = deg2rad(5);
+
+P_0 = diag([100 100 100 100 100 100]);
+
+Phi_0 = eye(6);
+Phi_0 = reshape(Phi_0, 36, 1);
+
+F = [0 0 n -1 0 0;
+    0 0 0 0 -1 0;
+    -n 0 0 0 0 -1;
+    0 0 0 0 0 0;
+    0 0 0 0 0 0;
+    0 0 0 0 0 0];
+
+H_matrix = eye(6);
+
+P_k = P_0;
+
+Q = zeros(6);
+
+y_k = [theta_1_0; theta_2_0; theta_3_0; omega_1_0; omega_2_0; omega_3_0; Hw_1_0; Hw_2_0; Hw_3_0; b_1_0; b_2_0; b_3_0; Phi_0];
+
+R = diag((deg2rad([0.1 0.1 0.1 0 0 0])).^2);
+
+theta_store = zeros(N-1, 3);
+b_store = zeros(N-1, 3);
+
+for k = 1:N-1
+
+    theta_store(k, :) = (y_k(1:3))';
+    b_store(k, :) = (y_k(10:12))';
+
+    attitude_measurement_error = normrnd(0, deg2rad(0.1), 1, 3);
+    
+    t_span = t2(k:k+1);
+    [t3, y_k1_k] = ode45(@(t, y)fun3(y, J, kp, kd, n, Md, H, attitude_measurement_error, F), t_span, y_k);
+    
+    y_k1_k = (y_k1_k(length(t3), :))';
+
+    x_k1_k = [y_k1_k(1:3); y_k1_k(10:12)];
+
+    theta_1_measured = y_k1_k(1) + attitude_measurement_error(1);
+    theta_2_measured = y_k1_k(2) + attitude_measurement_error(2);
+    theta_3_measured = y_k1_k(3) + attitude_measurement_error(3);
+    omega_measured = y_k1_k(4:6) + angular_velocity_measurement_errors;
+   
+    z_k1 = [theta_1_measured; theta_2_measured; theta_3_measured; omega_measured];
+
+    Phi_k1_k = reshape(y_k1_k(13:48), 6, 6);
+
+    P_k1_k = Phi_k1_k * P_k * Phi_k1_k' + Q;
+
+    K_k1_k = P_k1_k * H_matrix' * inv(H_matrix * P_k1_k * H_matrix' + R);
+    
+    z_estimated = [y_k1_k(1:3) + attitude_measurement_error'; y_k1_k(4:6) + y_k1_k(10:12)];
+
+    x_k1_k1 = x_k1_k + K_k1_k * (z_k1 - z_estimated);
+
+    P_k1_k1 = (eye(6) - K_k1_k*H_matrix) * P_k1_k;
+    
+    y_k1_k(1:3) = x_k1_k1(1:3);
+    y_k1_k(10:12) = x_k1_k1(4:6);
+    
+    y_k = y_k1_k;
+    P_k1_k = P_k1_k1;
+    
+
+
+end
+
+% Plot theta angles over time
+figure(3)
+dim = length(t2);
+plot(t2(1:dim-1), rad2deg(theta_store(:, 1)), LineWidth=2, Color='blue')
+hold on
+plot(t2(1:dim-1), rad2deg(theta_store(:, 2)), LineWidth=2, Color='red')
+plot(t2(1:dim-1), rad2deg(theta_store(:, 3)), LineWidth=2, Color='green')
+yline(0.1, Color='black', LineWidth=2)
+legend('\theta_1', '\theta_2', '\theta_3', '\theta = 0.1 deg', fontsize=15)
+xlabel('Integration time  [s]', FontSize=15)
+ylabel('Angle  [deg]', fontsize=15)
+ax = gca(figure(2));
+ax.FontSize = 15;
+grid("on")
+title('Performance of PD controller', FontSize=15)
+saveas(figure(3), 'task.7.pdf')
+hold off
+
+figure(4)
+dim = length(t2);
+plot(t2(1:dim-1), rad2deg(b_store(:, 1)), LineWidth=2, Color='blue')
+hold on
+plot(t2(1:dim-1), rad2deg(b_store(:, 2)), LineWidth=2, Color='red')
+plot(t2(1:dim-1), rad2deg(b_store(:, 3)), LineWidth=2, Color='green')
+yline(0.1, Color='blue', LineStyle='--' , LineWidth=2)
+yline(-0.1, Color='red', LineStyle='--', LineWidth=2)
+yline(0.15, Color='green', LineStyle='--', LineWidth=2)
+legend('b_1', 'b_2', 'b_3', 'b = 0.1 deg/s', 'b = -0.1 deg/s', 'b = 0.15 deg/s', fontsize=15)
+xlabel('Integration time  [s]', FontSize=15)
+ylabel('Angle  [deg]', fontsize=15)
+ax = gca(figure(2));
+ax.FontSize = 15;
+grid("on")
+title('Performance of PD controller', FontSize=15)
+hold off
 %% Functions
 
 function dy = fun(y, J, kp, kd, n, Md, H)
@@ -197,34 +305,52 @@ dy = [theta_dot_1; theta_dot_2; theta_dot_3; omega_dot_1; omega_dot_2; omega_dot
 
 end
 
-function dy = fun3(y, J, kp, kd, n, Md, H)
+function dy = fun3(y, J, kp, kd, n, Md, H, attitude_measurement_error, F)
 
+theta_1 = y(1);
+theta_2 = y(2);
+theta_3 = y(3);
+omega_1 = y(4);
+omega_2 = y(5);
+omega_3 = y(6);
+Hw_1 = y(7);
+Hw_2 = y(8);
+Hw_3 = y(9);
+b_1 = y(10);
+b_2 = y(11);
+b_3 = y(12);
+Phi = reshape(y(13:48), 6, 6);
+
+angular_velocity_measurement_errors = deg2rad([0.1, -0.1, 0.15]);
+
+omega_measured_1 = omega_1 + angular_velocity_measurement_errors(1);
+omega_measured_2 = omega_2 + angular_velocity_measurement_errors(2);
+omega_measured_3 = omega_3 + angular_velocity_measurement_errors(3);
+
+theta_dot_1 = omega_measured_1 - b_1 + n * theta_3;
+theta_dot_2 = omega_measured_2 - b_2 + n;
+theta_dot_3 = omega_measured_3 - b_3 - n * theta_1;
+
+theta_1_measured = theta_1 + attitude_measurement_error(1);
+theta_2_measured = theta_2 + attitude_measurement_error(2);
+theta_3_measured = theta_3 + attitude_measurement_error(3);
+
+Hw_dot_1 = kp(1) * theta_1_measured + kd(1) * theta_dot_1;
+Hw_dot_2 = kp(2) * theta_2_measured + kd(2) * theta_dot_2;
+Hw_dot_3 = kp(3) * theta_3_measured + kd(3) * theta_dot_3;
+
+omega_dot_1 = (1/J(1, 1)) * (Md(1) - 3 * n^2 * (J(2, 2) - J(3, 3)) * theta_1 - (J(2, 2) - J(3, 3)) * n * omega_3 - Hw_dot_1 - omega_3 * H - n*Hw_3);
+omega_dot_2 = (1/J(2, 2)) * (3 * n^2 *(J(3, 3) - J(1, 1)) * theta_2 + Md(2) - Hw_dot_2);
+omega_dot_3 = (1/J(3, 3)) * (Md(3) + omega_1 * H + n * Hw_1 - Hw_dot_3 - (J(1, 1) - J(2, 2))*n*omega_1);
+
+b_dot_1 = 0;
+b_dot_2 = 0;
+b_dot_3 = 0;
+
+Phi_dot = F*Phi;
+Phi_dot = reshape(Phi_dot, 36, 1);
+
+dy = [theta_dot_1; theta_dot_2; theta_dot_3; omega_dot_1; omega_dot_2; omega_dot_3; Hw_dot_1; Hw_dot_2; Hw_dot_3; b_dot_1; b_dot_2; b_dot_3; Phi_dot];
 
 end
 
-
-function EKF_single_step(x_0, state_transition_matrix, H_matrix, t_0, t_1, P_0)
-
-x_1 = x_0 + ode45(@(t,x) f(x, [t_0 t_1], x_0));
-Phi_1 = compute_state_transition_matrix(x1)
-P_1 = 
-K_1 = 
-x_11 = x_1 + K_1
-P_11
-end
-
-
-function compute_state_transition_matrix(x)
-
-end
-
-function compute_H_matrix(x)
-
-
-end
-
-
-function dx = f(x)
-
-
-end
